94645fe0e652a2242d1596eaa053e7f04f64fb21
deficrusader <159823641+deficrusader@users.noreply.github.com>
2024-05-31 22:29:37 +0200

    Update near.sh

--- near.sh
+++ near.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://near.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://near.lava.build/lava-referer-114bef37-c7b4-4752-bb3d-3ae62ec01bb8/"
 
 OUTPUT_DIR="logs"
 
